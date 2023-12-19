3eee3e6097f700f8b13ed832360d26f93cce57a8
C0A22113 <c0a221130b@edu.teu.ac.jp>
2023-12-19 16:53:00 +0900

    ランダムでトランプを表示

--- blackjack.py
+++ blackjack.py
@@ -2,12 +2,16 @@ import os
 import sys
 import math
 import pygame as pg
+import random
 
 WIDTH = 1600
 HEIGHT = 900
 MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
 
-class Card:
+class Card(pg.sprite.Sprite):
+    '''
+    カードに関するクラス
+    '''
     card = {
         "h":
             {
@@ -72,34 +76,71 @@ class Card:
                 "J": 'k11@2x.png',
                 "Q": 'k12@2x.png',
                 "K": 'k13@2x.png'
+            },
+        None:
+            {
+                None: "back@2x.png"
             }
         }
     
-    def __init__(self, s, r):
+    used_card = {}
+    def __init__(self, s: str, r: str, xy: tuple[int, int]):
+        '''
+        カード画像のSurfaceを生成する
+        引数1 s: カードの絵柄
+        引数2 r: カードの数字
+        引数3 xy: カード画像の位置座標タプル
+        '''
+        super().__init__()
         self.r = r
         self.s = s
-        self.img = pg.transform.rotozoom(pg.image.load(f'{MAIN_DIR}/playingcard-mini/{__class__.card[s][r]}'), 0, 2.0)
-        self.rct = self.img.get_rect()
-        self.rct.center = (800, 450)
-        
-    def update(self, screen: pg.Surface):
-        screen.blit(self.img, self.rct)
-        
+        self.image = pg.transform.rotozoom(pg.image.load(f'{MAIN_DIR}/playingcard-mini/{__class__.card[s][r]}'), 0, 1.5)
+        self.rect = self.image.get_rect()
+        self.rect.center = xy
     
+    def number(self) -> int:
+        '''
+        カードの数字を返す関数
+        戻り値 num: カードの数字
+        '''
+        if self.r == 'J' or self.r == 'Q' or self.r == 'K':
+            num = 10
+        elif self.r == 'A':
+            num = 1
+        return int(num)
+
+
 
 def main():
     pg.display.set_caption('black jack')
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    # 背景
-    card = Card("d",'A')
-    clock = pg.time.Clock()
+    screen.fill((70, 128, 79))
+    
+    player_cards = pg.sprite.Group()
+    dealer_cards = pg.sprite.Group()
+    suits = ['h', 's', 'd', 'k']
+    ranks = ["A","2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    p_card_suits = [random.choice(suits) for i in range(2)]
+    p_card_ranks = [random.choice(ranks) for i in range(2)]
+    d_card_suits = [random.choice(suits) for i in range(2)]
+    d_card_ranks = [random.choice(ranks) for i in range(2)]
+    
     tmr = 0
+    clock = pg.time.Clock()
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
             
-        card.update(screen)
+        for i in range(2):
+            player_cards.add(Card(p_card_suits[i], p_card_ranks[i], (750+100*i, 900-225)))
+        
+        dealer_cards.add(Card(d_card_suits[0], d_card_ranks[0], (750+100*0, 225)))
+        dealer_cards.add(Card(None, None, (750+100*1, 225)))
+        #cards.update(screen)
+        player_cards.draw(screen)
+        dealer_cards.draw(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
